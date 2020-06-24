0d87b55e703ebf511a1ea27ce69f50c31b358581
Hendrik Hein <hehein@microsoft.com>
2020-06-24 17:41:49 +0200

    updated train, test and score.py added utils.py to deployment

--- code/deploy/score.py
+++ code/deploy/score.py
@@ -1,44 +1,100 @@
+import json
+import logging
 import os
+
 import joblib
-import numpy as np
-import argparse
+import pandas as pd
 
-from sklearn.svm import SVC
-from azureml.core import Model
-from azureml.monitoring import ModelDataCollector
-from inference_schema.schema_decorators import input_schema, output_schema
-from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
-from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
+# from azureml.monitoring import ModelDataCollector
+from sktime.utils.load_data import from_long_to_nested
+
+from utils import create_response, get_connection_device_id
+
+TIMESERIESLENGTH = 10
 
 
-# The init() method is called once, when the web service starts up.
-# Typically you would deserialize the model file, as shown here using joblib,
-# and store it in a global variable so your run() method can access it later.
 def init():
     global model
-    global inputs_dc, prediction_dc
+    # global inputs_dc, prediction_dc
     # The AZUREML_MODEL_DIR environment variable indicates
     # a directory containing the model file you registered.
-    model_file_name = "model.pkl"
-    model_path = os.path.join(os.environ.get("AZUREML_MODEL_DIR"), model_file_name)
+    model_filename = "model.pkl"
+    model_path = os.path.join(os.environ["AZUREML_MODEL_DIR"], model_filename)
     model = joblib.load(model_path)
-    inputs_dc = ModelDataCollector("sample-model", designation="inputs", feature_names=["feat1", "feat2", "feat3", "feat4"])
-    prediction_dc = ModelDataCollector("sample-model", designation="predictions", feature_names=["prediction"])
+
+    logging.info("Model loaded.")
+
+    # inputs_dc = ModelDataCollector(
+    #     "sample-model",
+    #     designation="inputs",
+    #     feature_names=["feat1", "feat2", "feat3", "feat4"],
+    # )
+    # prediction_dc = ModelDataCollector(
+    #     "sample-model", designation="predictions", feature_names=["prediction"]
+    # )
 
 
-# The run() method is called each time a request is made to the scoring API.
-# Shown here are the optional input_schema and output_schema decorators
-# from the inference-schema pip package. Using these decorators on your
-# run() method parses and validates the incoming payload against
-# the example input you provide here. This will also generate a Swagger
-# API document for your web service.
-@input_schema('data', NumpyParameterType(np.array([[0.1, 1.2, 2.3, 3.4]])))
-@output_schema(StandardPythonParameterType({'predict': [['Iris-virginica']]}))
 def run(data):
-    # Use the model object loaded by init().
-    result = model.predict(data)
-    inputs_dc.collect(data) #this call is saving our input data into Azure Blob
-    prediction_dc.collect(result) #this call is saving our input data into Azure Blob
+    has_error = False
 
-    # You can return any JSON-serializable object.
-    return { "predict": result.tolist() }
+    logging.info("started run.")
+
+    # CONVERT STREAM ANALYTICS TO SKTIME FORMAT
+    logging.info("loading json.")
+    data = json.loads(data)
+    logging.info("json loaded.")
+
+    # Parse timestamps and temperature data
+    time_created_start = data.get("allevents")[0].get("timeCreated")
+    time_created_end = data.get("allevents")[-1].get("timeCreated")
+    temperature_data = [event.get("temperature") for event in data.get("allevents")]
+
+    logging.info(f"time_created_start: {time_created_start}")
+    logging.info(f"time_created_end: {time_created_end}")
+    logging.info(f"temperature_data: {temperature_data}")
+
+    # Check connection_device_id
+    connection_device_id, has_error, error_message = get_connection_device_id(data)
+    if has_error:
+        return create_response(has_error=has_error, error_message=error_message)
+
+    # Assert time series has at least TIMESERIESLENGTH elements
+    if len(temperature_data) < TIMESERIESLENGTH:
+        error_message = f"Time series of length {len(temperature_data)} does not have enough samples ({TIMESERIESLENGTH} samples required)."
+        logging.warning(error_message)
+        return create_response(has_error=True, error_message=error_message)
+
+    # Convert data to sktime format
+    case_id, dim_id = 0, 0
+    try:
+        long_data = [
+            [case_id, dim_id, reading_id, reading_data]
+            for reading_id, reading_data in enumerate(
+                temperature_data[-TIMESERIESLENGTH:]
+            )
+        ]
+    except Exception as e:
+        error_message = (
+            f"Could not convert dataset to long format due to exception: '{e}'"
+        )
+        logging.error(error_message)
+        return create_response(has_error=True, error_message=error_message)
+
+    # Predict
+    long_df = pd.DataFrame(
+        long_data, columns=["case_id", "dim_id", "reading_id", "value"]
+    )
+    sktime_df = from_long_to_nested(long_df)
+    prediction = model.predict(sktime_df).tolist()[0]
+
+    # inputs_dc.collect(sktime_df)  # this call is saving our input data into Azure Blob
+    # prediction_dc.collect(
+    #     prediction
+    # )  # this call is saving our input data into Azure Blob
+
+    return create_response(
+        prediction=prediction,
+        connection_device_id=connection_device_id,
+        time_created_start=time_created_start,
+        time_created_end=time_created_end,
+    )
